be027a468b26d1634deeebebf9392baa5d08c11e
cyr <kohanite60@gmail.com>
2026-08-27 23:41:57 +0800

    几个实体类的构建

---
+++ ai-rent-platform/src/main/java/org/example/airentplatform/demos/web/pojo/AiTask.java
@@ -0,0 +1,10 @@
+package org.example.airentplatform.demos.web.pojo;
+
+public class AiTask {
+    private int id;
+    private String taskname;
+    private String username;
+    private String status;
+    private String prompt;
+
+}
---
+++ ai-rent-platform/src/main/java/org/example/airentplatform/demos/web/pojo/Order.java
@@ -0,0 +1,10 @@
+package org.example.airentplatform.demos.web.pojo;
+
+public class Order {
+
+    private String user;
+    private String product;
+    private int money;
+
+
+}
